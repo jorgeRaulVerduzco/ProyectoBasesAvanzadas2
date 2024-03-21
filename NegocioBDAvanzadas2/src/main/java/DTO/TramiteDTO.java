5dd449405272db0ce47f58b2f8b6f5ef0a25dc34
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-03-20 19:58:40 -0700

    TramiteDTO
    O

--- NegocioBDAvanzadas2/src/main/java/DTO/TramiteDTO.java
+++ NegocioBDAvanzadas2/src/main/java/DTO/TramiteDTO.java
@@ -4,10 +4,75 @@
  */
 package DTO;
 
+import java.util.Calendar;
+
 /**
  *
  * @author INEGI
  */
 public class TramiteDTO {
-    
+    private Long id;
+    private float costo;
+    private Calendar fechaTramite;
+    private PersonaDTO persona;
+    private Calendar fechaVigencia;
+
+    public TramiteDTO(float costo, Calendar fechaTramite, PersonaDTO persona, Calendar fechaVigencia) {
+        this.costo = costo;
+        this.fechaTramite = fechaTramite;
+        this.persona = persona;
+        this.fechaVigencia = fechaVigencia;
+    }
+
+ 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+
+    public Calendar getFechaTramite() {
+        return fechaTramite;
+    }
+
+    public void setFechaTramite(Calendar fechaTramite) {
+        this.fechaTramite = fechaTramite;
+    }
+
+    public PersonaDTO getPersona() {
+        return persona;
+    }
+
+    public void setPersona(PersonaDTO persona) {
+        this.persona = persona;
+    }
+
+    public Calendar getFechaVigencia() {
+        return fechaVigencia;
+    }
+
+    public void setFechaVigencia(Calendar fechaVigencia) {
+        this.fechaVigencia = fechaVigencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Tramite{" +
+                "id=" + id +
+                ", costo=" + costo +
+                ", fechaTramite=" + fechaTramite +
+                ", persona=" + persona +
+                ", fechaVigencia=" + fechaVigencia +
+                '}';
+    }
 }
