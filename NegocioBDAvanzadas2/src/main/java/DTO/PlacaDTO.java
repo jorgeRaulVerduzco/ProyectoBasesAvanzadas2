556860204dd652ccdcc75d6b9e719fab97822dd3
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-03-20 20:03:25 -0700

    PLACADTO
    SI

--- NegocioBDAvanzadas2/src/main/java/DTO/PlacaDTO.java
+++ NegocioBDAvanzadas2/src/main/java/DTO/PlacaDTO.java
@@ -4,10 +4,68 @@
  */
 package DTO;
 
+import java.util.Calendar;
+
 /**
  *
  * @author INEGI
  */
-public class PlacaDTO {
-    
+public class PlacaDTO extends TramiteDTO {
+
+    private Long id;
+    private String digitosPlaca;
+    private String estado;
+    private AutomovilDTO automovil;
+
+    public PlacaDTO(String digitosPlaca, String estado, AutomovilDTO automovil, float costo, Calendar fechaTramite, PersonaDTO persona, Calendar fechaVigencia) {
+        super(costo, fechaTramite, persona, fechaVigencia);
+        this.digitosPlaca = digitosPlaca;
+        this.estado = estado;
+        this.automovil = automovil;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDigitosPlaca() {
+        return digitosPlaca;
+    }
+
+    public void setDigitosPlaca(String digitosPlaca) {
+        this.digitosPlaca = digitosPlaca;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public AutomovilDTO getAutomovil() {
+        return automovil;
+    }
+
+    public void setAutomovil(AutomovilDTO automovil) {
+        this.automovil = automovil;
+    }
+
+    @Override
+    public String toString() {
+        return "PlacaDto{"
+                + "id=" + id
+                + ", digitosPlaca='" + digitosPlaca + '\''
+                + ", estado='" + estado + '\''
+                + ", automovil=" + automovil
+                + ", costo=" + getCosto()
+                + ", fechaTramite=" + getFechaTramite().getTime()
+                + ", fechaVigencia=" + getFechaVigencia().getTime()
+                + '}';
+    }
 }
