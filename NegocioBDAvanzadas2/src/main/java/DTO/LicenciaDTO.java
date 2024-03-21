5b3c57d3ac5f3c7d60bf4631d509def3fe38a387
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-03-20 20:10:42 -0700

    licenciaDTO
    UFF

--- NegocioBDAvanzadas2/src/main/java/DTO/LicenciaDTO.java
+++ NegocioBDAvanzadas2/src/main/java/DTO/LicenciaDTO.java
@@ -4,10 +4,49 @@
  */
 package DTO;
 
+import Dominio.TipoLicencia;
+import java.util.Calendar;
+
 /**
  *
  * @author INEGI
  */
-public class LicenciaDTO {
-    
+public class LicenciaDTO extends TramiteDTO{
+      private int añosVigencia;
+    private TipoLicencia tipoLicencia;
+
+    public LicenciaDTO(int añosVigencia, TipoLicencia tipoLicencia, float costo, Calendar fechaTramite, PersonaDTO persona, Calendar fechaVigencia) {
+        super(costo, fechaTramite, persona, fechaVigencia);
+        this.añosVigencia = añosVigencia;
+        this.tipoLicencia = tipoLicencia;
+    }
+
+    // Getters y setters
+
+    public int getAñosVigencia() {
+        return añosVigencia;
+    }
+
+    public void setAñosVigencia(int añosVigencia) {
+        this.añosVigencia = añosVigencia;
+    }
+
+    public TipoLicencia getTipoLicencia() {
+        return tipoLicencia;
+    }
+
+    public void setTipoLicencia(TipoLicencia tipoLicencia) {
+        this.tipoLicencia = tipoLicencia;
+    }
+
+    @Override
+    public String toString() {
+        return "LicenciaDto{" +
+                "añosVigencia=" + añosVigencia +
+                ", tipoLicencia=" + tipoLicencia +
+                ", costo=" + getCosto() +
+                ", fechaTramite=" + getFechaTramite().getTime() +
+                ", fechaVigencia=" + getFechaVigencia().getTime() +
+                '}';
+    }
 }
