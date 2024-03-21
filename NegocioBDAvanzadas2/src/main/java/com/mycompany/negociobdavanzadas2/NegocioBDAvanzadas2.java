20503788f75dd75964023a5a3d4fd342876de493
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-03-20 18:17:00 -0700

    ojo
    ojastro

--- NegocioBDAvanzadas2/src/main/java/com/mycompany/negociobdavanzadas2/NegocioBDAvanzadas2.java
+++ NegocioBDAvanzadas2/src/main/java/com/mycompany/negociobdavanzadas2/NegocioBDAvanzadas2.java
@@ -11,6 +11,6 @@ package com.mycompany.negociobdavanzadas2;
 public class NegocioBDAvanzadas2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("no se cambia nada");
     }
 }
